6e881775908d75b905ea71e42ae181b03d968faf
Nguyen Hung Tam <tamnh@Nguyens-MacBook-Pro-2.local>
2017-11-20 07:35:05 +0800

    Implement random attributes

--- match_mpi.c
+++ match_mpi.c
@@ -18,7 +18,7 @@ const int MAX_RUN = 10;
 const int NUM_FIELD_PROCESS = 12;
 const int TEAM_SIZES = 11;
 const int NUM_ROUNDS = 2700;
-const int BUFFER_SIZE = 10;
+const int BUFFER_SIZE = 7;
 const int NUM_PROCESSES = 34;
 
 const int TEAM_A_FIRST_ID = 12;
@@ -28,9 +28,10 @@ const int TEAM_B_LAST_ID = 33;
 
 const int GOAL_RANGE[2] = {43, 51};
 
+const int ATTRIBUTE_LOWER_BOUND = 2;
+
 //unique key to create communication channels
 const int REPORT_COMM_KEY = 100;
-
 
 
 const int ATTRIBUTES_LIST[22][3] = {
@@ -63,16 +64,14 @@ struct PlayerInfo {
 	int id;
 	int prev_position[2];
 	int current_position[2];
+	int reached_ball_this_round;
 	int win_ball_this_round;
-	int num_meters_run;
-	int num_times_reached_ball;
-	int num_times_won_ball;
+	int ball_challenge;
 	int dribbling_skill;
 	int speed_skill;
 	int kick_power;
-	int ball_challenge;
-	int reached_ball_this_round;
 };
+
 
 bool is_first_half = true;
 
@@ -104,9 +103,22 @@ int absolute(int x) {
 }
 
 
+void assign_random_attributes(struct PlayerInfo * player_info) {
+	//from 2 to 8
+	int range = 6;
+	int dribbling_skill = 2 + rand() % (range + 1);
+	range = min(range, 15 - dribbling_skill - 4);
+	int kick_power = 2 + rand() % (range + 1);
+	int speed_skill = 15 - dribbling_skill - kick_power;
+
+	player_info->kick_power = kick_power;
+	player_info->speed_skill = speed_skill;
+	player_info->dribbling_skill = dribbling_skill;
+}
+
 //helper functions
 void print_player_info(struct PlayerInfo * player_info) {
-	printf("%d %d %d %d %d %d %d\n", player_info->prev_position[0], player_info->prev_position[1], player_info->current_position[0], player_info->current_position[1], 
+	printf("%d %d %d %d %d %d %d %d\n", player_info->id, player_info->prev_position[0], player_info->prev_position[1], player_info->current_position[0], player_info->current_position[1], 
 		player_info->reached_ball_this_round, player_info->win_ball_this_round, player_info->ball_challenge);
 }
 
@@ -152,17 +164,21 @@ void init_player(struct PlayerInfo * player_info, int rank) {
 	player_info->prev_position[1] = 0;
 	init_random_position(player_info->current_position);
 	
-	player_info->num_meters_run = 0;
-	player_info->num_times_reached_ball = 0;
-	player_info->num_times_won_ball = 0;
+	// player_info->num_meters_run = 0;
+	// player_info->num_times_reached_ball = 0;
+	// player_info->num_times_won_ball = 0;
 	player_info->win_ball_this_round = 0;
 	player_info->reached_ball_this_round = 0;
 
+	/*
 	int attr_idx = rank - NUM_FIELD_PROCESS;
 	player_info->speed_skill = ATTRIBUTES_LIST[attr_idx][0];
 	player_info->dribbling_skill = ATTRIBUTES_LIST[attr_idx][1];
 	player_info->kick_power = ATTRIBUTES_LIST[attr_idx][2];
+	*/
 
+	assign_random_attributes(player_info);
+	printf("%d %d %d %d\n", rank, player_info->dribbling_skill, player_info->speed_skill, player_info->kick_power);
 
 	//get player id 
 	if (is_team_A_player(rank)) {
@@ -219,10 +235,8 @@ void load_player_info_from_buffer(int * buffer, struct PlayerInfo * player_info)
 	player_info->current_position[0] = buffer[2];
 	player_info->current_position[1] = buffer[3];
 	player_info->reached_ball_this_round = buffer[4];
-	player_info->num_meters_run = buffer[5];
-	player_info->num_times_reached_ball = buffer[6];
-	player_info->num_times_won_ball = buffer[7];
-	player_info->win_ball_this_round = buffer[8];
+	player_info->win_ball_this_round = buffer[5];
+	player_info->ball_challenge = buffer[6];
 }
 
 
@@ -232,10 +246,8 @@ void load_player_info_into_buffer(int * buffer, struct PlayerInfo * player_info)
 	buffer[2] = player_info->current_position[0];
 	buffer[3] = player_info->current_position[1];
 	buffer[4] = player_info->reached_ball_this_round;
-	buffer[5] = player_info->num_meters_run;
-	buffer[6] = player_info->num_times_reached_ball;
-	buffer[7] = player_info->num_times_won_ball;
-	buffer[8] = player_info->win_ball_this_round;
+	buffer[5] = player_info->win_ball_this_round;
+	buffer[6] = player_info->ball_challenge;
 }
 
 
@@ -326,9 +338,7 @@ void perform_kick(int rank, struct PlayerInfo * player_info, int * ball_position
 	}
 
 	target_goal[1] = player_info->current_position[1];
-	target_goal[1] = min(GOAL_RANGE[1], target_goal[1]);
-	target_goal[1] = max(GOAL_RANGE[0], target_goal[1]);
-
+	target_goal[1] = GOAL_RANGE[0] + rand() % (GOAL_RANGE[1] - GOAL_RANGE[0] + 1);	
 	move_to_point(ball_position, target_goal, 2 * player_info->kick_power);
 }
 
@@ -348,18 +358,16 @@ void join_challenge_phase(int rank, int field_id, struct PlayerInfo * player_inf
 		//update reaching count
 		player_info->reached_ball_this_round = 1;
 		
-
-		player_info->ball_challenge = 1 + rand() % 9;
-		player_info->ball_challenge *= player_info->dribbling_skill;
+		ball_challenge = 1 + rand() % 9;
+		ball_challenge *= player_info->dribbling_skill;
 		//load the buffer
-
-		buffer[1] = player_info->ball_challenge;
-
+		buffer[1] = ball_challenge;
 		perform_kick(rank, player_info, ball_position);
 		buffer[2] = ball_position[0];
 		buffer[3] = ball_position[1];
 	}
 
+	player_info->ball_challenge = ball_challenge;
 	
 	MPI_Gather(buffer, BUFFER_SIZE, MPI_INT, NULL, BUFFER_SIZE, MPI_INT, 0, *subfield_comm);
 
@@ -385,7 +393,6 @@ void run_player_round(int rank, struct PlayerInfo * player_info, int * buffer, i
 	player_info->reached_ball_this_round = 0;
 	player_info->ball_challenge = -1;
 
-
 	MPI_Bcast(ball_position, 2, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
 
@@ -396,7 +403,7 @@ void run_player_round(int rank, struct PlayerInfo * player_info, int * buffer, i
 	move_to_point(player_info->current_position, ball_position, min(player_info->speed_skill, MAX_RUN));
 
 	//update distance run
-	player_info->num_meters_run += get_distance(player_info->prev_position, player_info->current_position);
+	//player_info->num_meters_run += get_distance(player_info->prev_position, player_info->current_position);
 
 	int field_id = get_field_index(player_info->current_position);
 	MPI_Comm_split(MPI_COMM_WORLD, field_id, rank, subfield_comm);
@@ -408,7 +415,6 @@ void run_player_round(int rank, struct PlayerInfo * player_info, int * buffer, i
 	MPI_Comm_free(subfield_comm);
 	// printf("%d %d %d\n", rank, ball_position[0], ball_position[1]);
 	send_player_info_to_field_process(buffer, player_info, report_comm);
-
 	MPI_Bcast(reset, 1, MPI_INT, 0, *report_comm);
 }
 
@@ -522,6 +528,8 @@ void run_field_round(int round, int rank, struct PlayerInfo * players_info, int 
 			ball_position[0] = 64;
 			ball_position[1] = 48;
 		}
+
+		printf("Current score: %d %d\n", score[0], score[1]);
 	}
 }
 
@@ -602,7 +610,7 @@ int main(int argc,char *argv[])
 			reset = 1;
 		}
 
-		run_process_round(round, rank, players_info, buffers, ball_position, &all_subfields_comm, &subfield_comm, &report_comm, &reset, &winner, score);
+		// run_process_round(round, rank, players_info, buffers, ball_position, &all_subfields_comm, &subfield_comm, &report_comm, &reset, &winner, score);
 		round ++;
 	}
 
@@ -620,6 +628,10 @@ int main(int argc,char *argv[])
 	*/
 
 	MPI_Finalize();
+
+	free(flat_buffer);
+	free(buffers);
+	free(players_info);
 
 	return 0;
 }
