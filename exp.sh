0a90877cda528081b6c21307da37acfef239cdee
tamnh <nguyenhungtam1994@gmail.com>
2017-11-20 10:55:38 +0800

    Update experiment results

--- exp.sh
+++ exp.sh
@@ -3,5 +3,5 @@ make
 rm -f training.lab.o
 rm -f match.lab.o
 
-mpirun -np 12 ./training_mpi >> training.lab.o
-mpirun -np 43 ./match_mpi >> match.lab.o
+time mpirun -np 12 ./training_mpi >> training.lab.o
+time mpirun -np 34 ./match_mpi >> match.lab.o
